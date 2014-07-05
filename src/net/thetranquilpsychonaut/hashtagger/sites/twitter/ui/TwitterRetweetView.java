5c09f277a96b2d689f5620f124e2c34c98644c34
Jaydeep Joshi <itwenty@leviathan-lin>
2014-07-05 18:47:32 +0530

    Make TwitterRetweetView inherit from PersonView

--- src/net/thetranquilpsychonaut/hashtagger/sites/twitter/ui/TwitterRetweetView.java
+++ src/net/thetranquilpsychonaut/hashtagger/sites/twitter/ui/TwitterRetweetView.java
@@ -2,49 +2,38 @@ package net.thetranquilpsychonaut.hashtagger.sites.twitter.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import com.squareup.picasso.Picasso;
-import net.thetranquilpsychonaut.hashtagger.R;
 import net.thetranquilpsychonaut.hashtagger.sites.twitter.retrofit.pojos.Status;
+import net.thetranquilpsychonaut.hashtagger.sites.ui.PersonView;
 
 /**
  * Created by itwenty on 7/3/14.
  */
-public class TwitterRetweetView extends RelativeLayout
+public class TwitterRetweetView extends PersonView
 {
-    private ImageView retweetImage;
-    private TextView  retweetName;
-    private TextView  retweetScreenName;
-
     public TwitterRetweetView( Context context )
     {
-        this( context, null, 0 );
+        super( context );
     }
 
     public TwitterRetweetView( Context context, AttributeSet attrs )
     {
-        this( context, attrs, 0 );
+        super( context, attrs );
     }
 
     public TwitterRetweetView( Context context, AttributeSet attrs, int defStyle )
     {
         super( context, attrs, defStyle );
-        inflate( context, R.layout.twitter_retweet_view, this );
-        retweetImage = ( ImageView ) findViewById( R.id.retweet_image );
-        retweetName = ( TextView ) findViewById( R.id.retweet_name );
-        retweetScreenName = ( TextView ) findViewById( R.id.retweet_screen_name );
     }
 
-    public void update( Status status )
+    @Override
+    protected String getPersonImageUrl( Object result )
     {
-        Picasso.with( getContext() )
-                .load( status.getUser().getProfileImageUrl() )
-                .fit()
-                .centerCrop()
-                .into( retweetImage );
-        retweetName.setText( status.getUser().getName() );
-        retweetScreenName.setText( "@" + status.getUser().getScreenName() );
+        return ( ( Status ) result ).getUser().getProfileImageUrl();
+    }
+
+    @Override
+    protected String getPersonName( Object result )
+    {
+        return ( ( Status ) result ).getUser().getName();
     }
 }
